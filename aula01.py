12acaf87b8ed960ad70afd4fafe069064d4737ab
Jefferson Silva <jefferson.santossilva22@gmail.com>
2025-02-04 22:09:34 -0300

    Envio do projeto

---
+++ aula01.py
@@ -0,0 +1,11 @@
+print('Hello word')
+idade = 35
+print(idade)
+print('Testando o python')
+
+#Criando anbiente virtual env
+# python -m venv .venv
+#git init
+#git status
+#git add .
+# criar um arquivo .gitignore e adicionar o que não quer versionar
