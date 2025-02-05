271e455762349679a4a95d961c4e224f48fb17a9
Jefferson Silva <jefferson.santossilva22@gmail.com>
2025-02-04 22:10:14 -0300

    Segundo envio

--- aula01.py
+++ aula01.py
@@ -2,10 +2,16 @@ print('Hello word')
 idade = 35
 print(idade)
 print('Testando o python')
-
+print('Alterando o status do git')
 #Criando anbiente virtual env
 # python -m venv .venv
 #git init
 #git status
 #git add .
+#git commit -m "tanto faz"
 # criar um arquivo .gitignore e adicionar o que não quer versionar
+#Para dar o primeiro commit 
+#git config --global user.name "Jeff Silva"
+#git config --global user.email jefferson.santossilva22@gmail.com
+
+
